ec33d07d90ba48de8179552733d8718eef304434
Rahul Kini <93133233+RaXKini@users.noreply.github.com>
2024-05-18 21:44:36 +0530

    Update server.js
    Re-Correction

--- server.js
+++ server.js
@@ -1,153 +1,114 @@
-const http = require('http');
-require('dotenv').config();
-const express = require('express');
-const bodyParser = require('body-parser');
-const socketio = require('socket.io');
-const helmet = require('helmet');
-const nocache = require('nocache');
-const nanoid = require('nanoid').nanoid;
+require("dotenv").config();
+const express = require("express");
+const helmet = require("helmet");
+const bodyParser = require("body-parser");
+const expect = require("chai");
+const socket = require("socket.io");
 
-const {
-  playerJoin,
-  getPlayers,
-  playerLeave,
-  setPlayerState,
-} = require('./utils/players');
-import Collectible from './public/Collectible.mjs';
-import gameConfig from './public/gameConfig.mjs';
-import generateStartPos from './public/utils/generateStartPos.mjs';
-
-const fccTestingRoutes = require('./routes/fcctesting.js');
-const runner = require('./test-runner.js');
+const fccTestingRoutes = require("./routes/fcctesting.js");
+const runner = require("./test-runner.js");
+const cors = require("cors");
 
 const app = express();
-const server = http.createServer(app);
-const io = socketio(server);
+// security
+app.use(helmet.noSniff());
+app.use(helmet.xssFilter());
+app.use(helmet.noCache());
+app.use(helmet.hidePoweredBy({ setTo: "PHP 7.4.3" }));
 
-app.use(
-  helmet({
-    noSniff: true,
-    xssFilter: true,
-    hidePoweredBy: {
-      setTo: 'PHP 7.4.3',
-    },
-  })
-);
-app.use(nocache());
-
-app.use('/public', express.static(process.cwd() + '/public'));
-app.use('/assets', express.static(process.cwd() + '/assets'));
+app.use("/public", express.static(process.cwd() + "/public"));
+app.use("/assets", express.static(process.cwd() + "/assets"));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors({ origin: "*" }));
 
 // Index page (static HTML)
-app.route('/').get(function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.route("/").get(function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
-// For FCC testing purposes
+//For FCC testing purposes
 fccTestingRoutes(app);
 
 // 404 Not Found Middleware
 app.use(function (req, res, next) {
-  res.status(404).type('text').send('Not Found');
-});
-
-// Randomly generate collectible item's position
-const playField = gameConfig.playField;
-const collectibleSprite = gameConfig.collectibleSprite;
-const collectiblePos = generateStartPos(playField, collectibleSprite);
-// Instantiate a collectible item object
-const collectible = new Collectible({
-  x: collectiblePos.x,
-  y: collectiblePos.y,
-  id: nanoid(),
-  // Randomly select a collectible item sprite
-  spriteSrcIndex: Math.floor(Math.random() * collectibleSprite.srcs.length),
-});
-
-// Run when client connects
-io.on('connection', (socket) => {
-  socket.on('joinGame', (player) => {
-    const currentPlayers = getPlayers();
-    // Send opponents to the client
-    socket.emit('currentOpponents', currentPlayers);
-    // Send the collectible item to the client
-    socket.emit('collectible', collectible);
-
-    const currentPlayer = playerJoin(player);
-    // Broadcast the new player
-    socket.broadcast.emit('newOpponent', currentPlayer);
-  });
-
-  socket.on('playerStateChange', (player) => {
-    const updatedPlayer = setPlayerState(player);
-    // Broadcast the updated player info
-    socket.broadcast.emit('opponentStateChange', updatedPlayer);
-  });
-
-  socket.on('playerCollideWithCollectible', (player) => {
-    // Update player's score
-    player.score += collectible.value;
-    // Send the updated score to the client
-    socket.emit('scored', player.score);
-
-    const updatedPlayer = setPlayerState(player);
-    // Broadcast the updated player
-    socket.broadcast.emit('opponentStateChange', updatedPlayer);
-
-    // Generate new position for the collectible item
-    let newCollectiblePos = generateStartPos(playField, collectibleSprite);
-    // Regenerate the new position if it is the same with the previous
-    // position
-    while (
-      newCollectiblePos.x === collectible.x &&
-      newCollectiblePos.y === collectible.y
-    ) {
-      newCollectiblePos = generateStartPos(playField, collectibleSprite);
-    }
-
-    const newCollectibleId = nanoid();
-    // Select the next collectible item sprite
-    const newCollectibleSpriteSrcIndex =
-      collectible.spriteSrcIndex === collectibleSprite.srcs.length - 1
-        ? 0
-        : collectible.spriteSrcIndex + 1;
-    // Update collectible item's state
-    collectible.setState({
-      x: newCollectiblePos.x,
-      y: newCollectiblePos.y,
-      id: newCollectibleId,
-      spriteSrcIndex: newCollectibleSpriteSrcIndex,
-    });
-    // Send the new(updated) collectible item to all clients
-    io.sockets.emit('collectible', collectible);
-  });
-
-  // Run when player disconnects
-  socket.on('disconnect', () => {
-    const player = playerLeave(socket.id);
-    socket.broadcast.emit('opponentLeave', player.id);
-  });
+  res.status(404).type("text").send("Not Found");
 });
 
 const portNum = process.env.PORT || 3000;
 
 // Set up server and tests
-server.listen(portNum, () => {
+const server = app.listen(portNum, () => {
   console.log(`Listening on port ${portNum}`);
-  if (process.env.NODE_ENV === 'test') {
-    console.log('Running Tests...');
+  if (process.env.NODE_ENV === "test") {
+    console.log("Running Tests...");
     setTimeout(function () {
       try {
         runner.run();
       } catch (error) {
-        console.log('Tests are not valid:');
+        console.log("Tests are not valid:");
         console.error(error);
       }
     }, 1500);
   }
 });
 
-module.exports = server; // For testing
+const connections = [];
+const io = socket(server);
+let allPlayers = [];
+let goal = {};
+
+io.sockets.on("connection", (socket) => {
+  connections.push(socket);
+
+  io.emit("connected", {
+    msg: `Connected ${socket.id}`,
+    connections: connections.length,
+  });
+
+  // add player to list of allPlayers
+  socket.on("init", (data) => {
+    // and associate the socket id
+    data.localPlayer.socketId = socket.id;
+    // only if they aren't in the list of allPlayers
+    if (
+      !allPlayers.includes(
+        allPlayers.find(
+          (player) => player.socketId === data.localPlayer.socketId
+        )
+      )
+    ) {
+      allPlayers.push(data.localPlayer);
+    }
+    io.emit("updateClientPlayers", { allPlayers, goal });
+  });
+
+  socket.on("updateServerPlayers", (data) => {
+    // update goal from client
+    goal = data.goal;
+    // get index of player who triggered update
+    const player = allPlayers.findIndex(
+      (player) => player.id == data.localPlayer.id
+    );
+    // update player by index
+    allPlayers[player].x = data.localPlayer.x;
+    allPlayers[player].y = data.localPlayer.y;
+    allPlayers[player].score = data.localPlayer.score;
+
+    io.emit("updateClientPlayers", { allPlayers, goal });
+  });
+
+  // remove player from list of all players by associated the socket id
+  socket.on("disconnect", () => {
+    connections.splice(connections.indexOf(socket), 1);
+    allPlayers = allPlayers.filter((player) => player.socketId !== socket.id);
+    io.emit("updateClientPlayers", { allPlayers, goal });
+    socket.broadcast.emit("disconnected", {
+      msg: `${socket.id} disconnected`,
+      connections: connections.length,
+    });
+  });
+});
+
+module.exports = app; // For testing
