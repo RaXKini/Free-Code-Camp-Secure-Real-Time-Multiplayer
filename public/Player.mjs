e86cb4815f3097bd77ea1c458dd16a53cf8f2b70
Rahul Kini <93133233+RaXKini@users.noreply.github.com>
2024-05-18 21:47:55 +0530

    Update Player.mjs
    Re-Correction

--- public/Player.mjs
+++ public/Player.mjs
@@ -1,119 +1,63 @@
-import gameConfig from './gameConfig.mjs';
-
-const playField = gameConfig.playField;
-const playFieldWidth = playField.width;
-const playFieldHeight = playField.height;
-const playFieldOffsetLeft = gameConfig.padding;
-const playFieldOffsetTop = gameConfig.infoFieldHeight;
-
-const avatar = gameConfig.avatar;
-const avatarWidth = avatar.width;
-const avatarHeight = avatar.height;
-
-const collectibleSprite = gameConfig.collectibleSprite;
-const collectibleSpriteWidth = collectibleSprite.width;
-const collectibleSpriteHeight = collectibleSprite.height;
+import { dimensions, arena } from "./gameCanvas.mjs";
 
 class Player {
-  constructor({ x, y, score = 0, id }) {
+  constructor({ x, y, score = 0, id, local = false }, size = 10) {
     this.x = x;
     this.y = y;
     this.score = score;
     this.id = id;
-    this.speed = 5;
-    this.dir = null;
+    this.size = size;
+    this.local = local;
   }
 
-  draw(ctx, image) {
-    // If user hits one of the control key,
-    // update player's position every frame
-    if (this.dir) {
-      this.movePlayer(this.dir, this.speed);
-    }
-
-    const x = this.x + playFieldOffsetLeft;
-    const y = this.y + playFieldOffsetTop;
-    ctx.drawImage(image, x, y, avatarWidth, avatarHeight);
+  draw() {
+    const ctx = dimensions.context;
+    ctx.fillStyle = this.local ? "green" : "purple";
+    ctx.fillRect(this.x, this.y, this.size, this.size);
   }
 
-  movePlayer(dir, speed) {
-    switch (dir) {
-      case 'up':
-        this.y = this.y - speed >= 0 ? this.y - speed : 0;
-        break;
-      case 'down':
-        this.y =
-          this.y + speed <= playFieldHeight - avatarHeight
-            ? this.y + speed
-            : playFieldHeight - avatarHeight;
-        break;
-      case 'right':
-        this.x =
-          this.x + speed <= playFieldWidth - avatarWidth
-            ? this.x + speed
-            : playFieldWidth - avatarWidth;
-        break;
-      case 'left':
-        this.x = this.x - speed >= 0 ? this.x - speed : 0;
-      default:
-        this.x = this.x;
-        this.y = this.y;
-    }
-  }
-
-  collision(collectible) {
-    // Get top left coordinate of player's avatar
-    // bounding rectangle
-    const l1 = {
-      x: this.x,
-      y: this.y,
-    };
-    // Get bottom right coordinate of player's avatar
-    // bounding rectangle
-    const r1 = {
-      x: this.x + avatarWidth,
-      y: this.y + avatarHeight,
-    };
-
-    // Get top left coordinate of collectible item's sprite
-    // bounding rectangle
-    const l2 = {
-      x: collectible.x,
-      y: collectible.y,
-    };
-    // Get bottom right coordinate of collectible item's sprite
-    // bounding rectangle
-    const r2 = {
-      x: collectible.x + collectibleSpriteWidth,
-      y: collectible.y + collectibleSpriteHeight,
-    };
-
-    // If one rectangle is on left side of other
-    if (l1.x >= r2.x || l2.x >= r1.x) {
-      return false;
-    }
-
-    // If one rectangle is above other
-    if (r1.y <= l2.y || r2.y <= l1.y) {
-      return false;
-    }
-
-    return true;
-  }
-
-  calculateRank(players) {
-    const numOfPlayers = players.length;
-    let rank;
-
-    if (this.score === 0) {
-      rank = numOfPlayers;
+  movePlayer(dir, speed = 10) {
+    // set direction based on key control
+    const directionKeyMap = { w: "up", s: "down", a: "left", d: "right" }[dir];
+    // move and limit coordinates based on direction map
+    const movement = {
+      up: Math.max(dimensions.minY, this.y - speed),
+      down: Math.min(dimensions.maxY - speed, this.y + speed),
+      left: Math.max(dimensions.minX, this.x - speed),
+      right: Math.min(dimensions.maxX - speed, this.x + speed),
+    }[directionKeyMap];
+    // set appropriate axis
+    if (directionKeyMap == "up" || directionKeyMap == "down") {
+      this.y = movement;
     } else {
-      const sortedPlayers = players.sort(
-        (playerA, playerB) => playerB.score - playerA.score
-      );
-      rank = sortedPlayers.findIndex((player) => player.id === this.id) + 1;
+      this.x = movement;
     }
-    return `Rank: ${rank} / ${numOfPlayers}`;
+    // console.log(`${this.x}, ${this.y}`);
+  }
+
+  collision(item) {
+    const xCollision = !(
+      this.x > item.x + item.size || this.x + this.size < item.x
+    );
+    const yCollision = !(
+      this.y > item.y + item.size || this.y + this.size < item.y
+    );
+    // if collision
+    if (xCollision && yCollision) {
+      this.score = this.score + 1;
+      return true;
+    }
+    return false;
+  }
+
+  calculateRank(arr) {
+    const totalPlayers = arr.length;
+    const rankedListings = arr.sort((a, b) => (a.score < b.score ? 1 : -1));
+    const currentRanking = rankedListings.findIndex((p) => p.id == this.id) + 1;
+    const rankingText = `Rank: ${currentRanking}/${totalPlayers}`;
+    // set ranking text
+    arena.drawRank(rankingText);
+    return `Rank: ${currentRanking}/${totalPlayers}`;
   }
 }
 
